a877c595a2c9e70a02cfc5179a2e1a00b2250373
yi sun <yisun@yis-MacBook-Pro.local>
2019-07-31 12:09:56 -0700

    test postgres and cassandra

--- server/s3.env.js
+++
@@ -1,8 +0,0 @@
-const env = {
-    AWS_ACCESS_KEY: 'AKIAJO2TMKEUHDBJM7XQ',
-    AWS_SECRET_ACCESS_KEY: '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',
-    REGION: 'us-east-2',
-    Bucket: 'scarfly'
-};
-
-module.exports = env;
